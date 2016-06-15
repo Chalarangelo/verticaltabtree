cc03258427c8502b4eb65368672e18c127384f69
Chalarangelo <chalarangelo@gmail.com>
2016-06-15 19:27:02 +0300

    Empty sidebar with toggle, icons

--- verticaltabtree.js
+++ verticaltabtree.js
@@ -1,9 +1,48 @@
-var self = require("sdk/self");
+var tabtreebar = require("sdk/ui/sidebar").Sidebar({
+  	id: 'verticaltabtree-sidebar',
+  	title: 'Vertical Tab Tree',
+  	url: require("sdk/self").data.url("verticaltabtree_sidebar.html"),
+  	onAttach: function (worker) {
+  		worker.port.emit("vtt init");
+    	worker.port.on("ping", function() {
+    	});
+    },
+    onDetach: function (worker) {
+  		worker.port.emit("vtt dispose");
+    	worker.port.on("ping", function() {
+    	});
+    },
+    onShow: function (worker) {
+  		worker.port.emit("vtt start_update");
+    	worker.port.on("ping", function() {
+    	});
+    },
+    onHide: function (worker) {
+  		worker.port.emit("vtt stop_update");
+    	worker.port.on("ping", function() {
+    	});
+    }
+});
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+var tabtreebartoggle = require('sdk/ui/button/action').ActionButton({
+	id: 'verticaltabtree-toggle',
+	label: 'Open/Close Vertical Tab Tree Sidebar',
+	icon: {
+		"16": "./icon-16.png",
+    	"32": "./icon-32.png",
+    	"64": "./icon-64.png"
+	},
+	onClick: toggleSidebar
+});
+
+var tabtreebarOpen = false;
+function toggleSidebar(state){
+	if (!tabtreebarOpen){
+		tabtreebar.show();
+		tabtreebarOpen = true;
+	}
+	else{
+		tabtreebar.hide();
+		tabtreebarOpen = false;
+	}
 }
-
-exports.dummy = dummy;
