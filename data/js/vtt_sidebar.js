dc286383d7dbdbc3b2e8e170a766ab072aaa4154
Chalarangelo <chalarangelo@gmail.com>
2016-06-28 21:03:00 +0300

    Proper sidebar init, proper updating on tab load

--- data/js/vtt_sidebar.js
+++ data/js/vtt_sidebar.js
@@ -1,5 +1,20 @@
+// Fires when a tab's content is altered (new page loaded).
 addon.port.on("vtt alter", function(tabData) {
+	if($('#'+tabData.id).length == 0){
+		$('#tabList').append('<li id="'+tabData.id+'"><span class="fav" id="fav'+tabData.id+'"></span>'+tabData.title+'</li>');
+		$('#fav'+tabData.id).css('background-image','url('+tabData.favicon+')');
+	}
+	else{
+		$('#'+tabData.id).html('<span class="fav" id="fav'+tabData.id+'"></span>'+tabData.title);
+		$('#fav'+tabData.id).css('background-image','url('+tabData.favicon+')');
+	}
+});
+// Fires once when the sidebar is opened.
+addon.port.on("vtt clear", function(){
+	$('#tabList').html('');
+});
+// Fires once for each open tab when the sidebar is opened.
+addon.port.on("vtt init", function(tabData){
 	$('#tabList').append('<li id="'+tabData.id+'"><span class="fav" id="fav'+tabData.id+'"></span>'+tabData.title+'</li>');
-	console.log('favicon: '+tabData.favicon);
 	$('#fav'+tabData.id).css('background-image','url('+tabData.favicon+')');
 });
--- verticaltabtree.js
+++ verticaltabtree.js
@@ -1,25 +1,34 @@
-var sidebarworker;
-
+var sidebarworker;				// Worker for the sidebar's async port messaging.
+var tabs = require("sdk/tabs");	// Tabs list.
+// Helper function that returns the favicon using google. 
+// See https://github.com/Chalarangelo/verticaltabtree/issues/4 for more
+// details.
+function getFaviconFromDomain(url){
+	return('https://plus.google.com/_/favicon?domain_url='+url);
+}
+// Sidebar control for the add-on.
 var tabtreebar = require("sdk/ui/sidebar").Sidebar({
   	id: 'verticaltabtree-sidebar',
   	title: 'Vertical Tab Tree',
   	url: require("sdk/self").data.url("verticaltabtree_sidebar.html"),
+  	// When a worker is attached to the sidebar, store the worker in the variable.
   	onAttach: function (worker) {
   		sidebarworker = worker;
-  		sidebarworker.port.emit("vtt init");
     },
+    // When the worker is detached from the sidebar, reset the worker.
     onDetach: function () {
-    	sidebarworker.port.emit("vtt dispose");
   	 	sidebarworker = undefined;	
     },
+    // When the sidebar is shown, clear previous content and send initialization 
+    // messages to populate the tabs list.
     onShow: function () {
-  		sidebarworker.port.emit("vtt start_update");
-    },
-    onHide: function () {
-  		worker.port.emit("vtt stop_update");
+    	sidebarworker.port.emit("vtt clear");
+    	for (let tab of tabs)
+    		sidebarworker.port.emit("vtt init",{id: tab.id, title: tab.title, favicon: getFaviconFromDomain(tab.url)});
+  		
     }
 });
-
+// Menu button that opens and closes the sidebar.
 var tabtreebartoggle = require('sdk/ui/button/action').ActionButton({
 	id: 'verticaltabtree-toggle',
 	label: 'Open/Close Vertical Tab Tree Sidebar',
@@ -30,8 +39,9 @@ var tabtreebartoggle = require('sdk/ui/button/action').ActionButton({
 	},
 	onClick: toggleSidebar
 });
-
+// State of the sidebar (false for hidden).
 var tabtreebarOpen = false;
+// Change sidebar's hidden/shown state.
 function toggleSidebar(state){
 	if (!tabtreebarOpen){
 		tabtreebar.show();
@@ -42,31 +52,28 @@ function toggleSidebar(state){
 		tabtreebarOpen = false;
 	}
 }
-
-var tabs = require("sdk/tabs");
+// Fires when a tab is opened.
 tabs.on('open',function(tab){
 	console.log('opened '+tab.url);
 });
-
+// Fires when a tab is closed.
 tabs.on('close',function(tab){
 	console.log('closed '+tab.url);
 });
-
+// Fires when a tab is loaded.
 tabs.on('ready',function(tab){
 	console.log('loaded '+tab.url);
 	if(sidebarworker){
-		require("sdk/places/favicon").getFavicon(tab).then(function (url) {
-      		sidebarworker.port.emit("vtt alter",{id: tab.id, title: tab.title, favicon: url});
-    	});
-		
+		console.log('sent');
+    	sidebarworker.port.emit("vtt alter",{id: tab.id, title: tab.title, favicon: getFaviconFromDomain(tab.url)});
 	}
 });
-
+// Fires when a tab is activated.
 tabs.on('activate',function(tab){
 	console.log('active changed to '+tab.url);
 	
 });
-
+// Fires when a tab is deactivated.
 tabs.on('deactivate',function(tab){
 	console.log('active changed from '+tab.url);
 });
